1297b80c31797966515dabf3bce3de18bf93e711
Ruslan Mansurov <ruslanways@gmail.com>
2023-01-11 15:06:35 +0100

    refactored validator.js

--- static/validator.js
+++ static/validator.js
@@ -1,8 +1,8 @@
 function validateForm() {
   const dates = document.querySelectorAll(".form-control");
   if (
-    Date.parse(dates[1].value) < Date.parse(dates[0].value) ||
-    Date.parse(dates[3].value) < Date.parse(dates[2].value)
+    dates[1].valueAsNumber < dates[0].valueAsNumber ||
+    dates[3].valueAsNumber < dates[2].valueAsNumber
   ) {
     alert("Departure date must be later than arrival date!\nPlease correct.");
     return false;
