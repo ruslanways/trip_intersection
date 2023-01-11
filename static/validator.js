f2241547f7390e421c52be3f166d3de9a4d6e531
Ruslan Mansurov <ruslanways@gmail.com>
2023-01-11 12:32:12 +0100

    added newline symbol in alert js

--- static/validator.js
+++ static/validator.js
@@ -4,7 +4,7 @@ function validateForm() {
     Date.parse(dates[1].value) < Date.parse(dates[0].value) ||
     Date.parse(dates[3].value) < Date.parse(dates[2].value)
   ) {
-    alert("Departure date must be later than arrival date! Please correct.");
+    alert("Departure date must be later than arrival date!\nPlease correct.");
     return false;
   }
 }
